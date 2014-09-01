413cd16e76b2eea91290e7ba6f27cee92fa02d34
Jimmy Pace <pacejimmy@yahoo.com>
2014-09-01 00:40:52 -0400

    Add emc trick.

---
+++ add_emc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+#UNTESTED:  Should make emc be an alias for emacsclient
+
+echo '===> UNTESTED: You should verify this works.'
+echo 'alias emc='emacsclient'' >> ~/.bash_profile
+source ~/.bash_profile
