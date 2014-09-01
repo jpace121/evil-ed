cc76f11293eeedbeaa7a7d4a316bfe32e1ce3161
Jimmy Pace <pacejimmy@yahoo.com>
2014-09-01 00:42:01 -0400

    add add_emd comment

--- add_emc.sh
+++ add_emc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-#UNTESTED:  Should make emc be an alias for emacsclient
+#UNTESTED:  Should make emc be an alias for emacsclient for ease of typing when
+# emacs --daemon has already been started.
 
 echo '===> UNTESTED: You should verify this works.'
 echo 'alias emc='emacsclient'' >> ~/.bash_profile
