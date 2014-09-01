e49f107c83c6b963815c5d11235dbc732f4099f3
Jimmy Pace <pacejimmy@yahoo.com>
2014-09-01 01:08:29 -0400

    Add -t option.

--- add_emc.sh
+++ add_emc.sh
@@ -4,5 +4,5 @@
 # emacs --daemon has already been started.
 
 echo '===> UNTESTED: You should verify this works.'
-echo 'alias emc='emacsclient --alternate-editor=""'' >> ~/.bash_profile
+echo 'alias emc='emacsclient -t --alternate-editor=""'' >> ~/.bash_profile
 source ~/.bash_profile
