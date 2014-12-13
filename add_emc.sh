69da3796d7f4f1f00d86a87084583ff7551e663e
James Pace <jpace121@gmail.com>
2014-12-13 18:29:14 +0000

    Fix add-emc bug.

--- add_emc.sh
+++ add_emc.sh
@@ -3,6 +3,5 @@
 #UNTESTED:  Should make emc be an alias for emacsclient for ease of typing when
 # emacs --daemon has already been started.
 
-echo '===> UNTESTED: You should verify this works.'
-echo 'alias emc='emacsclient -t --alternate-editor=""'' >> ~/.bash_profile
-source ~/.bash_profile
+echo '===> Aliasing emc. May need to run source once done.'
+echo 'alias emc='\''emacsclient -t --alternate-editor=""'\''' >> ~/.bash_profile
