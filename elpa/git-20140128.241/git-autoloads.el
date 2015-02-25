797537a8b72aab02bc9decf1af6b94ddcecdbab9
Jimmy Pace <pacejimmy@yahoo.com>
2015-02-24 20:33:28 -0500

    Half way try to fix git.

---
+++ elpa/git-20140128.241/git-autoloads.el
@@ -0,0 +1,18 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("git-pkg.el" "git.el") (21739 60173 67022
+;;;;;;  0))
+
+;;;***
+
+(provide 'git-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-autoloads.el ends here
