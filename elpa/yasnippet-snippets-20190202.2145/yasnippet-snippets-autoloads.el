d8e276c21855479e309b3466f0f908e409135cac
James Pace <jpace121@gmail.com>
2019-02-07 23:16:26 -0500

    Get snippets. Ignore security file.

---
+++ elpa/yasnippet-snippets-20190202.2145/yasnippet-snippets-autoloads.el
@@ -0,0 +1,29 @@
+;;; yasnippet-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "yasnippet-snippets" "yasnippet-snippets.el"
+;;;;;;  (23645 463 601209 453000))
+;;; Generated autoloads from yasnippet-snippets.el
+
+(autoload 'yasnippet-snippets-initialize "yasnippet-snippets" "\
+Load the `yasnippet-snippets' snippets directory.
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(yasnippet-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("yasnippet-snippets-pkg.el") (23645 468
+;;;;;;  925225 357000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; yasnippet-snippets-autoloads.el ends here
