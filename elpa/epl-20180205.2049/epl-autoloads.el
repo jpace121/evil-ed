6c45fe8f3f265d1bdcd99bcaff0ae2789689565b
James Pace <jpace121@gmail.com>
2019-02-07 23:01:59 -0500

    Update elpa.

---
+++ elpa/epl-20180205.2049/epl-autoloads.el
@@ -0,0 +1,15 @@
+;;; epl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("epl.el") (23644 65047 317203 867000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epl-autoloads.el ends here
