0cc008afa4cef4f3ade3462d25402fa511b66522
James Pace <jpace121@gmail.com>
2019-12-03 03:01:13 +0000

    Update all of the things.

--- elpa/f-20190109.906/f-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("f.el") (23644 65042 973131 876000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; f-autoloads.el ends here
---
+++ elpa/parseclj-20190531.711/parseclj-autoloads.el
@@ -0,0 +1,16 @@
+;;; parseclj-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("parseclj-ast.el" "parseclj-lex.el" "parseclj-parser.el"
+;;;;;;  "parseclj-pkg.el" "parseclj.el") (24037 52387 735520 666000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; parseclj-autoloads.el ends here
