15755a4aa32dbd1403b2b8e326ddcadcc2b93a4e
James Pace <jpace121@gmail.com>
2021-04-19 01:17:09 +0000

    Update packages.

--- elpa/a-20180907.953/a-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; a-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("a.el") (24037 52387 39511 188000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; a-autoloads.el ends here
