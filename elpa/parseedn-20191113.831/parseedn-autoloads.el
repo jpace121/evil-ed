3c64482158375ee8c9ae406e27c6ff261e544553
James Pace <jpace121@gmail.com>
2022-05-07 17:48:36 -0400

    Delete elpa and lang tool before replacement with straight.

--- elpa/parseedn-20191113.831/parseedn-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; parseedn-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("parseedn.el") (24037 52388 151526 325000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; parseedn-autoloads.el ends here
